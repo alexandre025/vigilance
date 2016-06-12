717788d7df547780fc3c22207a57bf9afccd6260
alexandre025 <alexandre.ferraille@gmail.com>
2016-06-12 11:38:04 +0200

    Proper devise routes naming

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'register'
+  }
 
   resources :organizations do
     resources :tags
