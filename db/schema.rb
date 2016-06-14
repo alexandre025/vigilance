8ebdeb7741b0616924fbab226a49f752bccc3d54
alexandre025 <alexandre.ferraille@gmail.com>
2016-06-14 23:53:35 +0200

    Login, register, reset pwd styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614162627) do
+ActiveRecord::Schema.define(version: 20160614212233) do
 
   create_table "assignments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -100,6 +100,8 @@ ActiveRecord::Schema.define(version: 20160614162627) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "auth_token"
+    t.string   "firstname"
+    t.string   "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
