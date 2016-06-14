a29760db60de80dca641f77d9d236bf844451713
alexandre025 <alexandre.ferraille@gmail.com>
2016-06-14 11:13:54 +0200

    postfix test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-      location: '/usr/sbin/sendmail',
-      arguments: '-i -t'
-  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: 'localhost', port: 25, enable_starttls_auto: false}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'alexandre-ferraille.fr', port: 3000 }
 end
