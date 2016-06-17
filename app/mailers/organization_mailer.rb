c069df1e9348faed92358d32231bab366b199927
alexandre025 <alexandre.ferraille@gmail.com>
2016-06-17 10:43:47 +0200

    last fixes

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -3,6 +3,6 @@ class OrganizationMailer < ApplicationMailer
     @user = assignment.user
     @assignment = assignment
     @url = organization_join_url(@assignment.organization, auth_token: @user.auth_token, assignment_token: @assignment.assignment_token)
-    mail(to: @user.email, subject: "Vigilance - You're invited in #{assignment.organization.name}")
+    mail(to: @user.email, subject: "Readis - You're invited in #{assignment.organization.name}")
   end
 end
